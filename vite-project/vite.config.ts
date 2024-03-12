180c7cf1a4648f6f2ce447df5c4acaeaea9baae9
@@@ <1094549944@qq.com>
2024-03-12 16:07:24 +0800

    feat:配置prettier eslint  stylelint 配置alias ，支持jsx

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,6 +1,8 @@
 // UserConfig,ConfigEnv 都是类型约束
 import { UserConfig, ConfigEnv, loadEnv, defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+// 配置vue使用jsx
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // 以下三项引入是为配置Element-plus自动按需导入
 import AutoImport from "unplugin-auto-import/vite";
@@ -17,8 +19,15 @@ const pathSrc = resolve(__dirname, "src");
 
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   return {
+    resolve: {
+      alias: {
+        "@": pathSrc,
+      },
+    },
     plugins: [
       vue(),
+      // jsx、tsx语法支持
+      vueJsx(),
       AutoImport({
         // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
         imports: ["vue", "pinia", "vue-router"],
@@ -26,6 +35,15 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
           // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
           ElementPlusResolver(),
         ],
+        eslintrc: {
+          enabled: true, //  默认 false, true 启用生成。生成一次就可以，避免每次工程启动都生成，一旦生成配置文件之后，最好把 enable 关掉，即改成 false。
+          //  否则这个文件每次会在重新加载的时候重新生成，这会导致 eslint 有时会找不到这个文件。当需要更新配置文件的时候，再重新打开
+          // 浏览器需要访问所有应用到 vue/element api 的页面才会生成所有自动导入 api 的文件 json
+          filepath: "./.eslintrc-auto-import.json",
+          // 默认就是 ./.eslintrc-auto-import.json
+          globalsPropValue: true,
+        },
+        vueTemplate: true, // 默认 true 是否在vue 模版中自动导入
         dts: resolve(pathSrc, "typings", "auto-import.d.ts"), //  自动导入组件类型声明文件位置，默认根目录
       }),
       Components({
